2fa32db554d16a157f850a4a3e5394d1c56bebfe
Eunsu Park <phd.choux@gmail.com>
2023-04-11 21:34:04 +0900

    chapter-02

---
+++ C/C_tutorial/chapter-02/Comment01/main.c
@@ -0,0 +1,16 @@
+// p.54
+#include <stdio.h>
+
+/*
+이 구간의 코드는 모두 주석문입니다.
+*/
+
+int main()
+{
+    //결과를 저장하는 변수입니다.
+    int Result = 10;
+    char CH = 'A'; //사용자 입력
+
+// printf("Hello, World!\n");
+    return 0;
+}
