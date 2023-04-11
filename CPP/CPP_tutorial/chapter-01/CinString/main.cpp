dc712018db5f087f9ad91db13e51d197c59b1b6f
Eunsu Park <phd.choux@gmail.com>
2023-04-11 19:37:49 +0900

    chapter-01

---
+++ CPP/CPP_tutorial/chapter-01/CinString/main.cpp
@@ -0,0 +1,18 @@
+// p.39
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+int main()
+{
+    std::string S = "Test string";
+    std::cout << "Sample string" << std::endl;
+    std::cout << S << std::endl;
+
+    S = "New string";
+    std::cout << S << std::endl;
+    
+    std::cout << "저는 " << 20 << "살" << "입니다." << std::endl;
+
+    return 0;
+}
