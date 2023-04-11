3b34e0d52dc3d0c7740d66a28733886d69103410
Eunsu Park <phd.choux@gmail.com>
2023-04-11 19:44:46 +0900

    chapter-01

--- CPP/CPP_tutorial/chapter-01/CinString/main.cpp
+++ CPP/CPP_tutorial/chapter-01/CinString/main.cpp
@@ -5,14 +5,21 @@
 
 int main()
 {
-    std::string S = "Test string";
-    std::cout << "Sample string" << std::endl;
-    std::cout << S << std::endl;
+    int Age;
+    std::cout << "나이를 입력하세요." << std::endl;
+    std::cin >> Age;
 
-    S = "New string";
-    std::cout << S << std::endl;
-    
-    std::cout << "저는 " << 20 << "살" << "입니다." << std::endl;
+    char Job[32];
+    std::cout << "직업을 입력하세요." << std::endl;
+    std::cin >> Job;
+
+    std::string Name;
+    std::cout << "이름을 입력하세요." << std::endl;
+    std::cin >> Name;
+
+    std::cout << "당신의 이름은 " << Name << " 이고, "
+        << "나이는 " << Age << " 살이며, "
+        << "직업은 " << Job << " 입니다." << std::endl;
 
     return 0;
 }
