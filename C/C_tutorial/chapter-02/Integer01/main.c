9bbb38c952caa1a586463c37f077dfdea5340f86
Eunsu Park <phd.choux@gmail.com>
2023-04-11 21:22:06 +0900

    chapter-02

---
+++ C/C_tutorial/chapter-02/Integer01/main.c
@@ -0,0 +1,21 @@
+// p.45
+#include <stdio.h>
+
+int main()
+{
+    char CH = 'A';
+    // wchar_t WCH = L'A';
+    short S = 10;
+    int I = 10;
+    long L = 10L;
+    long int LI = 10L;
+    long long int LLI = 10LL;
+
+    unsigned char UCH = 0;
+    unsigned short US = 65535U;
+    unsigned int UI = 1U;
+    unsigned long int ULI = 1UL;
+    unsigned long long int ULLI = 1ULL;
+
+    return 0;
+}
